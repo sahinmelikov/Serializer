7fe6c49621159b1f57080bb62c3fee5f43832e5a
sahinmelikov <1rima621melikov@gmail.com>
2023-05-02 10:18:13 +0400

    Serializer

---
+++ aa/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
+using System.Text.Json.Nodes;
+
+var myObject=new JsonObject();
+
+
+#region task1
+//string Path = @"C:/Users/555/OneDrive/Masaüstü/aa/Models";
+//string PathData = @"C:/Users/555/OneDrive/Masaüstü/aa/Data";
+string PathDataFile = @"C:/Users/555/OneDrive/Masaüstü/aa/Data/jsonData.json";
+
+
+//Directory.CreateDirectory(Path);
+//Directory.CreateDirectory(PathData);
+//File.Create(PathDataFile);
+
+#endregion
+
+
+
+
+
+#region Task2
+var urlJson = @"https://jsonplaceholder.typicode.com/posts";
+
+HttpClient httpClient = new HttpClient();
+var content = await httpClient.GetStringAsync(urlJson);  ///////<====bu hissede sehv var🤐
+
+Console.WriteLine(JsonConvert.SerializeObject(content));
+
+//using (StreamWriter sw = new StreamWriter(PathDataFile))
+//{
+//    sw.WriteLine(JsonConvert.SerializeObject(content));
+//}
+//using (StreamReader sr = new StreamReader(PathDataFile))
+//{
+//    string data = await sr.ReadToEndAsync();
+//    JsonConvert.DeserializeObject<JsonObject>(content);
+
+
+//}
+
+
+#endregion
+
+
